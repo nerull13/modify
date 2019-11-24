7b4647f34dfed20336211116b31fd6c14d690f72
nerull13 <diving-seller@hotmail.fr>
2019-11-24 23:20:27 +0100

    Create custom.menu.php

---
+++ custom.menu.php
@@ -0,0 +1,15 @@
+<?php
+
+$quickboxURL = "https://www.secretseed.net";
+$yggURL = "https://yggtorrent.com/";
+$torrent9URL = "https://www.torrent9.ch/";
+$cloudURL = "https://myseedbox.secretseed.net/nextcloud/";
+?> <!-- Do not remove this closing (?>) -->
+
+
+<li><a href="<?php echo "$quickboxURL"; ?>" class="grayscale" target="_blank"><i class="fa fa-heart text-danger"></i> <span><?php echo T('SecretSeed.Net'); ?></span></a></li>
+<li><a href="<?php echo "$yggURL"; ?>" class="grayscale" target="_blank"><i class="fa fa-chrome text-danger"></i> <span><?php echo T('YGG-Torrent'); ?></span></a></li>
+<li><a href="<?php echo "$torrent9URL"; ?>" class="grayscale" target="_blank"><i class="fa fa-chrome text-danger"></i> <span><?php echo T('Torrent9'); ?></span></a></li>
+<li><a href="<?php echo "$cloudURL"; ?>" class="grayscale" target="_blank"><img src="img/brands/nextcloud.png" class="brand-ico"><span><?php echo T(' myCloud'); ?></span></a></li>
+
+
