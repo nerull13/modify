49ab859a9cb455cc5481532d14581a2356fb1983
nerull13 <diving-seller@hotmail.fr>
2019-11-24 23:09:40 +0100

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,12 @@
+apt-get -yqq install git lsb-release
+adduser nerull
+sudo usermod -aG sudo nerull
+
+sudo openssl req -x509 -nodes -newkey rsa:2048 -keyout /etc/ssl/private/vsftpdserverkey.pem -out /etc/ssl/certs/vsftpdcertificate.pem -days 365
+echo "local_root=/home/$USER/torrents/rtorrent" >> /etc/vsftpd.conf
+echo "user_sub_token=$USER" >> /etc/vsftpd.conf
+
+sed -i -r "s/.*rsa_cert_file*/rsa_cert_file=/etc/ssl/certs/vsftpdcertificate.pem/g" /etc/vsftpd.conf
+sed -i -r "s/.*rsa_private_key_file*/rsa_private_key_file=/etc/ssl/private/vsftpdserverkey.pem/g" /etc/vsftpd.conf
+
+box install letsencrypt
