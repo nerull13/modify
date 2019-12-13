c7b0e5adfb9f27224c934ef9c7d5ecde06d02139
nerull13 <diving-seller@hotmail.fr>
2019-12-13 12:23:08 +0100

    maj
    maj

--- install.sh
+++ install.sh
@@ -20,6 +20,7 @@ cp plugins.ini /srv/rutorrent/conf
 cp access.ini /srv/rutorrent/conf
 
 cp logo-light.png /srv/rutorrent/home/img
+yes | cp -rf Logo/favicon/* /srv/rutorrent/home/img/favicon
 
 cp custom.menu.php /srv/rutorrent/home/custom/
 cp index.html /srv/rutorrent/
