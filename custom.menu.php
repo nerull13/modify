40750a7b42b377091e1a1db0ae12c6e33390dbda
nerull13 <diving-seller@hotmail.fr>
2019-12-13 12:14:59 +0100

    Update custom.menu.php

--- custom.menu.php
+++ custom.menu.php
@@ -1,13 +1,13 @@
 <?php
 
-$quickboxURL = "https://www.secretseed.net";
+$quickboxURL = "https://www.secretseed.net/mon-compte/";
 $yggURL = "https://yggtorrent.ws/";
 $torrent9URL = "https://www.torrent9.ch/";
 $cloudURL = "https://myseedbox.secretseed.net/nextcloud/";
 ?> <!-- Do not remove this closing (?>) -->
 
 
-<li><a href="<?php echo "$quickboxURL"; ?>" class="grayscale" target="_blank"><i class="fa fa-heart text-danger"></i> <span><?php echo T('SecretSeed.Net'); ?></span></a></li>
+<li><a href="<?php echo "$quickboxURL"; ?>" class="grayscale" target="_blank"><i class="fa fa-heart text-danger"></i> <span><?php echo T('Mon espace client'); ?></span></a></li>
 <li><a href="<?php echo "$yggURL"; ?>" class="grayscale" target="_blank"><i class="fa fa-chrome text-danger"></i> <span><?php echo T('YGG-Torrent'); ?></span></a></li>
 <li><a href="<?php echo "$torrent9URL"; ?>" class="grayscale" target="_blank"><i class="fa fa-chrome text-danger"></i> <span><?php echo T('Torrent9'); ?></span></a></li>
 <li><a href="<?php echo "$cloudURL"; ?>" class="grayscale" target="_blank"><img src="img/brands/nextcloud.png" class="brand-ico"><span><?php echo T(' myCloud'); ?></span></a></li>
