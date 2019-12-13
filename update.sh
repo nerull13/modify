fcff36141c55c1e84a9278f81c89947eff33c480
nerull13 <diving-seller@hotmail.fr>
2019-12-13 12:32:58 +0100

    Create update.sh

---
+++ update.sh
@@ -0,0 +1,13 @@
+cp plugins.ini /srv/rutorrent/conf
+cp access.ini /srv/rutorrent/conf
+
+cp logo-light.png /srv/rutorrent/home/img
+yes | cp -rf Logo/favicon/* /srv/rutorrent/home/img/favicon
+
+cp custom.menu.php /srv/rutorrent/home/custom/
+cp index.html /srv/rutorrent/
+
+nano /srv/rutorrent/home/inc/config.php
+
+
+
